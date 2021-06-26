63a2f204e44c3b476b076481c9dcd541ddcec743
Mário Rabelo <mutrano@gmail.com>
2021-06-26 16:59:47 -0300

    Corrige Serializacao do ErroPadrao

--- src/main/java/br/com/zupacademy/mario/transacoes/ErroPadrao.java
+++ src/main/java/br/com/zupacademy/mario/transacoes/ErroPadrao.java
@@ -5,12 +5,28 @@ public class ErroPadrao {
 	private Integer status;
 	private String caminho;
 	private String erro;
-
+	
 	public ErroPadrao(String mensagem, Integer status, String caminho, String erro) {
 		this.mensagem = mensagem;
 		this.status = status;
 		this.caminho = caminho;
 		this.erro = erro;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public String getCaminho() {
+		return caminho;
+	}
+
+	public String getErro() {
+		return erro;
 	}
 	
 
